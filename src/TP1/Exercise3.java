888ef3ba0a72b8333b480ed687ae7d4f5823e3dc
marouane_m7b <marouane.ma7boub@gmail.com>
2026-01-03 00:56:09 +0100

    Exercise 3 Enter: read a character string from the keyboard and store it in a variable

---
+++ src/TP1/Exercise3.java
@@ -0,0 +1,15 @@
+package TP1;
+
+import java.util.Scanner;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String text = "";
+
+        System.out.print("Enter a string: ");
+        text = input.nextLine();
+        System.out.println("String saved successfully!");
+
+    }
+}
