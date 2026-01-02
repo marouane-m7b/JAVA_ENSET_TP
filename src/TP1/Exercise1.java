c6686a2143c6400949228c0aaf1da3f6ab80948a
marouane_m7b <marouane.ma7boub@gmail.com>
2026-01-02 23:01:44 +0100

    Exercise 1 Sort and display the list of grades

---
+++ src/TP1/Exercise1.java
@@ -0,0 +1,35 @@
+package TP1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of students: ");
+        int n = scanner.nextInt();
+
+        double[] grades = new double[n];
+
+        System.out.println("Enter the grades:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Grade " + (i + 1) + ": ");
+            grades[i] = scanner.nextDouble();
+        }
+
+        sortAndDisplay(grades);
+
+        scanner.close();
+    }
+
+    public static void sortAndDisplay(double[] grades) {
+        double[] sortedGrades = grades.clone();
+        Arrays.sort(sortedGrades);
+        System.out.println("\nSorted grades:");
+        for (double grade : sortedGrades) {
+            System.out.print(grade + " ");
+        }
+        System.out.println();
+    }
+}
